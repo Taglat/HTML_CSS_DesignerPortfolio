3d83f06dd6c305f58bff1e7fc8614c12e6d772e7
Talgat Tokayev <ttokayev7@gmail.com>
2024-07-25 13:36:53 +0500

    hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './index.html';
 import './index.scss';
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav__list');
+
+if (hamburger && nav) {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        nav.classList.toggle('active');
+    })
+}
